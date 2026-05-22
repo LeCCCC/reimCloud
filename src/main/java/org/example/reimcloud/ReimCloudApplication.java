5c746fc0e0647f19259dfe1925ce11a488f410a7
Lecy Yang <isyujian.yang@gmail.com>
2026-05-22 11:21:56 +0800

    v2026.5.22 基础

---
+++ src/main/java/org/example/reimcloud/ReimCloudApplication.java
@@ -0,0 +1,13 @@
+package org.example.reimcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReimCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReimCloudApplication.class, args);
+    }
+
+}
